870641d239b1a2e3f79c4f756411900d15a1050f
carolinacee <108691793+carolinacee@users.noreply.github.com>
2022-09-28 19:05:16 -0400

    added forecast with dummy info and loop

--- src/code.js
+++ src/code.js
@@ -23,6 +23,28 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let forecastDays = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
+  forecastDays.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col">
+  <div class="weather-forecast-date">${day}</div>
+  <img src="http://openweathermap.org/img/wn/50d@2x.png"alt=""width="50px"/>
+  <div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max">18</span>
+  <span class="weather-forecast-temperature-min">12</span>
+  </div></div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -107,3 +129,5 @@ fahrenheitLink.classList.remove("active");
 let celsiusTemperature = null;
 
 searchCity("Tokyo");
+
+displayForecast();
